a817864f74ddac08591208fca494ecbcaa5d6fda
Tyler Chin <tyler.chin@mail.utoronto.ca>
2026-05-15 14:57:33 -0400

    fix: corrected import for linux systems

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Footer from '../widgets/Footer';
-import Header from '../widgets/header';
+import Header from '../widgets/Header';
 import PlaylistBar from '../widgets/PlaylistBar';
 import MyProjectContentContainer from '../widgets/MyProjectContentContainer';
 import AboutMeContentContainer from '../widgets/AboutMeContentContainer';
 import Div from '../components/html/Div';
-import './Home.css';
 import ClickToStart from '../widgets/ClickToStart';
+import './Home.css';
 
 export type View = 'projects' | 'about' | 'contact' | 'music' | 'food';
 
